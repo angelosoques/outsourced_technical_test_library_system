916710ed8d286f2a0b368da4c82308bfd754fb9d
Kenneth Ang <angelosoques@gmail.com>
2023-12-30 22:06:30 +0800

    added routes for members

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\LibraryController;
+use App\Http\Controllers\MemberController;
+use App\Models\Member;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +21,14 @@ use App\Http\Controllers\LibraryController;
 Route::post('/insert/library', [LibraryController::class, 'insertLibrary']);
 Route::post('/update/library', [LibraryController::class, 'updateLibrary']);
 
+//books
 Route::get('/book/{id}', [BookController::class, 'getSpecificBook']);
 Route::post('/insert/book', [BookController::class, 'insertBook']);
 Route::post('/update/book', [BookController::class, 'updateBook']);
-
 Route::get('/books/{libraryId}', [BookController::class, 'getAllBooks']);
+
+//members
+Route::get('/members/{libraryId}', [MemberController::class], 'getMembers');
+Route::get('/member/{id}', [MemberController::class, 'getSpecificMember']);
+Route::post('/insert/member', [MemberController::class, 'insertMember']);
+Route::post('/update/member', [MemberController::class, 'updateMember']);
